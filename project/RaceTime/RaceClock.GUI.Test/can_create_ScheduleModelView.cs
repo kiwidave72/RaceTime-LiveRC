3f9b1e8681de25335ffab09d0b396a46501708b2
kiwidave72 <kiwidave72@hotmail.com>
2015-01-20 21:26:03 +1300

    started creating practice app gui

---
+++ project/RaceTime/RaceClock.GUI.Test/can_create_ScheduleModelView.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Input;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaceTime.GUI;
+using RaceTime.Library.Model.Practice;
+
+namespace RaceClock.GUI.Test
+{
+    [TestClass]
+    public class can_create_ScheduleModelView
+    {
+        ScheduleModelView schedule ;
+
+        public can_create_ScheduleModelView()
+        {
+            schedule = new ScheduleModelView();
+
+            schedule.Model.Add(new PracticeClass("Super Stock Touring", 1000 * 10));
+
+            schedule.Model.Add(new PracticeClass("Modified Touring", 1000 * 10));
+
+            schedule.Model.Add(new PracticeClass("Pro10 and Pro12", 1000 * 10));
+
+            schedule.Model.Add(new PracticeClass("Formula One", 1000 * 10));
+
+            schedule.Model.Add(new PracticeClass("Mini's", 1000 * 10));
+
+            schedule.Model.Add(new PracticeClass("Under 12's", 1000 * 10));            
+        }
+
+        [TestMethod]
+        public void it_runs_like_this()
+        {
+            ICommand command = schedule.StartScheduleCommand;
+            command.Execute(null);
+
+            Assert.IsTrue(schedule.Model.IsScheduleRunning);
+        }
+
+        [TestMethod]
+        public void it_stops_like_this()
+        {
+            ICommand command = schedule.StartScheduleCommand;
+            command.Execute(null);
+
+
+            ICommand stopcommand = schedule.StopScheduleCommand;
+            stopcommand.Execute(null);
+
+            Assert.IsTrue(schedule.Model.IsScheduleRunning==false);
+        }
+    }
+}
---
+++ project/RaceTime/RaceTime.GUI/ScheduleModelView.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using RaceTime.Library.Annotations;
+using RaceTime.Library.Model;
+using RaceTime.Library.Model.Schedule;
+
+namespace RaceTime.GUI
+{
+    public class ScheduleModelView : INotifyPropertyChanged
+    {
+
+        private DefaultSchedule _model = new DefaultSchedule();
+
+
+        private Timer _timer;
+        private string _elapsedTime;
+
+        private DelegateCommand _stopScheduleCommand;
+        private DelegateCommand _startScheduleCommand;
+        private string _annoucementText;
+
+
+        public ScheduleModelView()
+        {
+            _startScheduleCommand = new DelegateCommand(StartSchedule);
+            _stopScheduleCommand = new DelegateCommand(StopSchedule);
+
+            _model.OnAnnouncement += OnAnnouncement;
+
+        }
+
+        public DefaultSchedule Model
+        {
+            get { return _model; }
+
+        }
+
+
+        public string ElapsedTime
+        {
+            get { return _elapsedTime; }
+            set
+            {
+                _elapsedTime = value;
+                OnPropertyChanged("ElapsedTime");
+            }
+        }
+
+        public String AnnouncmentText
+        {
+            get { return _annoucementText; }
+            set
+            {
+                _annoucementText = value;
+                OnPropertyChanged("AnnouncmentText");
+
+            }
+        }
+        
+        public ICommand StartScheduleCommand
+        {
+            get { return _startScheduleCommand; }
+        }
+
+        public ICommand StopScheduleCommand
+        {
+            get { return _stopScheduleCommand; }
+        }
+
+        
+
+        private void StartSchedule()
+        {
+            _timer = new Timer(UpdateTimer);
+            _timer.Change(100, 100);
+
+            _model.Run();
+        }
+
+        private void StopSchedule()
+        {
+            _timer.Dispose();
+
+            _model.Stop();
+        }
+
+
+        private void UpdateTimer(object state)
+        {
+            this.ElapsedTime = _model.RaceClock.ElapsedTimeString;
+
+        }
+
+        private void OnAnnouncement(object sender, Announcement e)
+        {
+            this.AnnouncmentText = e.Text;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
